bacf97fcb388b16076e011136171f3d0b68857f7
Julius <74532956+juliusvara@users.noreply.github.com>
2020-11-26 10:54:58 +0200

    initial JS

---
+++ js/kintamieji.js
@@ -0,0 +1,23 @@
+/* 
+    Kintamieji:
+
+    iniciavimo budai:
+    -const(default)
+    -let
+    -var(pasistengti nenaudoti)
+
+    kintamuju tipai:
+    -skaiciai(number)
+    -tekstas(string)
+    -logika(boolean)
+    -sarasas/masyvas(array)
+
+*/
+
+const vardas ='Vardenis';
+const metai ='2020';
+const arTiesa = true;
+const arNaktis = false;
+const pazymiai = [10, 2, 7, 6];
+const zodynas = ['labs', 'ryts', 'dasd']
+
---
+++ js/veiksmai.js
@@ -0,0 +1,47 @@
+/*  matematines operacijos */
+
+const skaicius1 = 5;
+const skaicius2 = 8;
+
+const suma = skaicius1 + skaicius2;
+
+console.log(suma);
+
+const skaicius3 = 88;
+const skaicius4 = 77;
+
+
+const zodis1 = 'labs';
+const zodis2 = 'ryts';
+const zodis3 = 'LT';
+
+// labs ryts, LT.
+
+// const sakinys = zodis1 + ' ' + zodis2 + ', ' + zodis3 + '.';
+// console.log(sakinys);
+/* tekstu iniciavimo subtilybes */
+
+const txt1 = "Lorem";
+const txt2 = 'lorem';
+
+
+const jsText = 'JS Rocks!';
+const emoji = '🚀 ✔';
+const htmlText = 'Lorem ipsum';
+
+const pilnasHTML = '<div>\
+<h1>JS Rocks!  🚀 ✔ </h1>\
+<p>Lorem ipsum</p>\
+</div>';
+
+console.log(pilnasHTML);
+
+const backtickHTML = `<div>
+<h1>${jsText} ${emoji}</h1>
+<p>${htmlText}</p>
+</div>`;
+console.log(backtickHTML);
+
+
+
+
