0e92134a8274f5cb5940a17e216d7c0d817aa116
Julius <74532956+juliusvara@users.noreply.github.com>
2020-11-30 13:04:56 +0200

    for n'stuff

---
+++ js/for.js
@@ -0,0 +1,16 @@
+/* 
+for ciklas
+*/
+console.log('-----------------------------');
+
+let money = 0;
+const pajamos = [100, 200, 300, 100, 100, 50, 100]
+
+
+console.log(money);
+
+
+for (let i= 0; i < pajamos.length; i++) {
+    money += pajamos[i];
+    console.log(money);
+}
